7a1a9eea25926d5cc1dcec3ec1d22b662ede90b1
Chang-Goo Kim <cgkim@astro.princeton.edu>
2023-11-11 21:03:30 -0500

    Ruff formatting

--- prfm/__init__.py
+++ prfm/__init__.py
@@ -2,3 +2,33 @@ __author__ = "Chang-Goo Kim"
 __email__ = "changgookim@gmail.com"
 __license__ = "MIT"
 __description__ = "Python packages for the PRFM theory"
+
+
+from prfm.prfm import (
+    get_scale_height,
+    get_weights,
+    get_weight_contribution,
+    get_feedback_yield,
+    get_feedback_yield_comp,
+    get_sfr,
+    get_pressure,
+    get_self_consistent_solution,
+    get_sigma_eff,
+    PRFM,
+)
+
+from prfm import simulations as sims
+
+__all__ = [
+    "get_scale_height",
+    "get_weights",
+    "get_weight_contribution",
+    "get_feedback_yield",
+    "get_feedback_yield_comp",
+    "get_sfr",
+    "get_pressure",
+    "get_self_consistent_solution",
+    "get_sigma_eff",
+    "PRFM",
+    "sims",
+]
